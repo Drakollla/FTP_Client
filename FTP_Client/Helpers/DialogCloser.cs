aa6b2aaaba927bec637ed0ddf271ec401077b434
Darkness_696 <darkness_696@mail.ru>
2025-10-03 19:07:21 +0300

    changed size of windows and buttons.

---
+++ FTP_Client/Helpers/DialogCloser.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace FTP_Client.Helpers
+{
+    public static class DialogCloser
+    {
+        public static readonly DependencyProperty DialogResultProperty =
+            DependencyProperty.RegisterAttached(
+                "DialogResult",
+                typeof(bool?),
+                typeof(DialogCloser),
+                new PropertyMetadata(DialogResultChanged));
+
+        public static void SetDialogResult(Window target, bool? value)
+        {
+            target.SetValue(DialogResultProperty, value);
+        }
+
+        private static void DialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window window && e.NewValue != null)
+                window.DialogResult = (bool?)e.NewValue;
+        }
+    }
+}
--- FTP_Client/Helpers/FtpConnectionSettings.cs
+++ FTP_Client/Helpers/FtpConnectionSettings.cs
@@ -1,48 +1,35 @@
 ﻿using FTP_Client.Models;
-using System;
-using System.Net;
 
 namespace FTP_Client.Helpers
 {
     public class FtpConnectionSettings : ObservableObject
     {
-        private string _serverAddress = "127.0.0.1";
-        public string ServerAddress
+        private string _host = "ftp.dlptest.com";
+        private int _port = 21;
+        private string _userName = "dlpuser";
+        private string _password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+        public string Host
         {
-            get => _serverAddress;
-            set => SetProperty(ref _serverAddress, value);
+            get => _host;
+            set => SetProperty(ref _host, value);
         }
 
-        private int _port = 21;
         public int Port
         {
             get => _port;
             set => SetProperty(ref _port, value);
         }
 
-        private string _userName = "test";
         public string Username
         {
             get => _userName;
             set => SetProperty(ref _userName, value);
         }
 
-        private string _password = "BeloJek";
         public string Password
         {
             get => _password;
             set => SetProperty(ref _password, value);
-        }
-
-        public FtpWebRequest CreateFtpRequest(string stringUriRequest)
-        {
-            var uriBuilder = new UriBuilder("ftp", ServerAddress, Port);
-
-            var request = (FtpWebRequest)WebRequest.Create(uriBuilder.ToString() + stringUriRequest);
-            request.Credentials = new NetworkCredential(Username, Password);
-
-
-            return request;
         }
     }
 }
