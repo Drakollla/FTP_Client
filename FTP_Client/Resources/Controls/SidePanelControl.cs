260875400491d98fe05747050d8f0b3907346ee9
Darkness_696 <darkness_696@mail.ru>
2025-10-04 18:49:57 +0300

    Changed expamder on custom sidePanelControl

---
+++ FTP_Client/Resources/Controls/SidePanelControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FTP_Client.Resources.Controls
+{
+    public class SidePanelControl : ContentControl
+    {
+        static SidePanelControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SidePanelControl),
+                new FrameworkPropertyMetadata(typeof(SidePanelControl)));
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(object), typeof(SidePanelControl), new PropertyMetadata(null));
+      
+        public static readonly DependencyProperty IsExpandedProperty =
+            DependencyProperty.Register("IsExpanded", typeof(bool), typeof(SidePanelControl), new PropertyMetadata(false));
+
+        public object Header
+        {
+            get { return GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public bool IsExpanded
+        {
+            get { return (bool)GetValue(IsExpandedProperty); }
+            set { SetValue(IsExpandedProperty, value); }
+        }
+    }
+}
